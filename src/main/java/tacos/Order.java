3d68a3b25f42c56bcfd743f66128bc8e5166cc74
xzAugenCabbage <1372899560@qq.com>
2023-05-30 12:38:25 +0800

    Ch-2.3

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,12 +1,10 @@
 package tacos;
 
-import lombok.Data;
-
 import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-
 import org.hibernate.validator.constraints.CreditCardNumber;
+import javax.validation.constraints.NotBlank;
+import lombok.Data;
 
 @Data
 public class Order {
@@ -23,15 +21,16 @@ public class Order {
     @NotBlank(message="State is required")
     private String state;
 
-    @NotBlank(message="Zip is required")
+    @NotBlank(message="Zip code is required")
     private String zip;
 
     @CreditCardNumber(message="Not a valid credit card number")
     private String ccNumber;
 
-    @Pattern(regexp="^(0[1-9]|1[0-2])([\\\\/])([1-9][0-9])$", message="Must be formatted MM/YY")
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+            message="Must be formatted MM/YY")
     private String ccExpiration;
 
-    @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
+    @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
 }
--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,28 +1,33 @@
+
 package tacos.web;
-
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.ui.Model;
-import tacos.Ingredient;
-import tacos.Ingredient.Type;
-import tacos.Taco;
-
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+import tacos.Ingredient;
+import tacos.Ingredient.Type;
+import tacos.Taco;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
 
-    @GetMapping
-    public String showDesignForm(Model model) {
+
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
         List<Ingredient> ingredients = Arrays.asList(
                 new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
                 new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
@@ -35,20 +40,40 @@ public class DesignTacoController {
                 new Ingredient("SLSA", "Salsa", Type.SAUCE),
                 new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
         );
+
         Type[] types = Ingredient.Type.values();
         for (Type type : types) {
             model.addAttribute(type.toString().toLowerCase(),
                     filterByType(ingredients, type));
         }
+    }
 
+    @GetMapping
+    public String showDesignForm(Model model) {
         model.addAttribute("design", new Taco());
-
         return "design";
     }
 
-    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
-        return ingredients.stream()
+    @PostMapping
+    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
+        // Save the taco design...
+        // We'll do this in chapter 3
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
+    }
+
+    private List<Ingredient> filterByType(
+            List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
     }
+
 }
+
